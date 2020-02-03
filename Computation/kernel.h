792c2c240d4f7bfc098575407068a76813517e6a
Vince <cakewithsteak@gmail.com>
2020-02-03 20:36:02 +0100

    Structural changes in device code to allow RTC

--- Computation/kernel.h
+++ Computation/kernel.h
@@ -1,6 +1,6 @@
 #pragma once
-#include <builtin_types.h>
+#include "kernel_types.h"
 
-using fpdist_t = int;
+
 
 void launch_kernel(float re0, float re1, float im0, float im1, float tolerance, fpdist_t maxIters, fpdist_t* minmaxOut, cudaSurfaceObject_t surface, int surfW, int surfH);
