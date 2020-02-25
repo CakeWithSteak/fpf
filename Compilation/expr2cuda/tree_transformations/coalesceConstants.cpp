d31863fa6e5ae54f61458078ead33f879a0cf540
Violet <cakewithsteak@gmail.com>
2020-02-25 18:19:36 +0100

    Fix some bugs with constant coalescing

--- Compilation/expr2cuda/tree_transformations/coalesceConstants.cpp
+++ Compilation/expr2cuda/tree_transformations/coalesceConstants.cpp
@@ -3,18 +3,21 @@
 
 ExpressionNode* traverse(ExpressionNode* node) {
     if(node->type() == NodeType::OPERATOR) {
+        for(auto& child : node->children)
+            child = traverse(child);
+
         //Merge neg(5) into -5
         if(node->getOperator()->name == "neg") {
-            auto* child = traverse(node->children[0]);
+            auto* child = node->children[0];
             if(child->type() == NodeType::CONSTANT) {
                 auto* cons = dynamic_cast<ConstantNode*>(child);
                 auto* merged = new ConstantNode(-cons->value());
-                delete child;
+                delete node;
                 return merged;
             }
         } else if(node->getOperator()->name == "+" || node->getOperator()->name == "-") { //Merge {5,0} + {0, 3} into {5, 3}
-            auto* child1 = traverse(node->children[0]);
-            auto* child2 = traverse(node->children[1]);
+            auto* child1 = node->children[0];
+            auto* child2 = node->children[1];
             if(child1->type() == NodeType::CONSTANT && child2->type() == NodeType::CONSTANT) {
                 auto* a = dynamic_cast<ConstantNode*>(child1);
                 auto* b = dynamic_cast<ConstantNode*>(child2);
@@ -23,13 +26,9 @@ ExpressionNode* traverse(ExpressionNode* node) {
                     merged = new ConstantNode(b->value() + a->value());
                 else
                     merged = new ConstantNode(b->value() - a->value());
-                delete child1;
-                delete child2;
+                delete node;
                 return merged;
             }
-        } else {
-            for(auto& child : node->children)
-                child = traverse(child);
         }
     }
     if(node->type() == NodeType::ROOT)
