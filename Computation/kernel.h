bae2d18ed9282f8279f757ce2e59a5f4ba2b546f
Vince <cakewithsteak@gmail.com>
2020-03-10 15:45:32 +0100

    Fix a thread allocation bug

--- Computation/kernel.h
+++ Computation/kernel.h
@@ -5,7 +5,12 @@
 #include <cuda_runtime.h>
 
 inline void launch_kernel(CUfunction kernel, float re0, float re1, float im0, float im1, float tolerance, fpdist_t maxIters, fpdist_t* minmaxOut, cudaSurfaceObject_t surface, int surfW, int surfH, float pre, float pim) {
+    constexpr unsigned int BLOCK_SIZE = 1024;
+
     auto tsquare = tolerance * tolerance;
     void* args[] = {&re0, &re1, &im0, &im1, &tsquare, &maxIters, &minmaxOut, &surface, &surfW, &surfH, &pre, &pim};
-    CUDA_SAFE(cuLaunchKernel(kernel, surfW * surfH, 1, 1, 1024, 1, 1, 0, nullptr, args, nullptr));
+    unsigned int numBlocks = (surfW * surfH) / BLOCK_SIZE;
+    if((surfW * surfH) % BLOCK_SIZE != 0)
+        ++numBlocks;
+    CUDA_SAFE(cuLaunchKernel(kernel, numBlocks, 1, 1, BLOCK_SIZE, 1, 1, 0, nullptr, args, nullptr));
 }
