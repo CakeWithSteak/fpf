265635ab44e24675658e9e5e5f54f5c1e1ecafba
Vince <cakewithsteak@gmail.com>
2020-03-15 17:16:54 +0100

    Remove unused variable from Julia kernel

--- Computation/distance_metrics/julia.cuh
+++ Computation/distance_metrics/julia.cuh
@@ -2,12 +2,10 @@
 #include "../kernel_types.h"
 
 __device__ __inline__ dist_t juliaDist(complex z, dist_t maxIters, complex p, float rsquare) {
-    float2 last = z;
     for(dist_t i = 0; i < maxIters; ++i) {
         if(z.x * z.x + z.y * z.y > rsquare)
             return i;
         z = F(z, p);
-        last = z;
     }
     return -1;
 }
