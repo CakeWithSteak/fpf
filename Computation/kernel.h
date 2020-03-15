eb37bc8526115ac5b4f2f463ed20330186311f09
Violet <cakewithsteak@gmail.com>
2020-03-15 12:16:28 +0100

    Minor refactoring

--- Computation/kernel.h
+++ Computation/kernel.h
@@ -4,13 +4,15 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
+inline unsigned int ceilDivide(unsigned int x, unsigned int y) {
+    return (x + y - 1) / y;
+}
+
 inline void launch_kernel(CUfunction kernel, float re0, float re1, float im0, float im1, float tolerance, fpdist_t maxIters, fpdist_t* minmaxOut, cudaSurfaceObject_t surface, int surfW, int surfH, float pre, float pim) {
     constexpr unsigned int BLOCK_SIZE = 1024;
 
     auto tsquare = tolerance * tolerance;
     void* args[] = {&re0, &re1, &im0, &im1, &tsquare, &maxIters, &minmaxOut, &surface, &surfW, &surfH, &pre, &pim};
-    unsigned int numBlocks = (surfW * surfH) / BLOCK_SIZE;
-    if((surfW * surfH) % BLOCK_SIZE != 0)
-        ++numBlocks;
+    unsigned int numBlocks = ceilDivide(surfW * surfH, BLOCK_SIZE);
     CUDA_SAFE(cuLaunchKernel(kernel, numBlocks, 1, 1, BLOCK_SIZE, 1, 1, 0, nullptr, args, nullptr));
 }
