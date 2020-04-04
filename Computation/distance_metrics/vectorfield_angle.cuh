8a4608b787c56eb5691d8f267e10b461747711a8
Vince <cakewithsteak@gmail.com>
2020-04-04 11:46:32 +0200

    Fix range for direction mode

--- Computation/distance_metrics/vectorfield_angle.cuh
+++ Computation/distance_metrics/vectorfield_angle.cuh
@@ -4,5 +4,8 @@
 
 __device__ __inline__ dist_t vfAngle(complex z, dist_t unused1, complex p, float unused2) {
     complex disp = csub(F(z, p), z);
-    return atan2f(disp.y, disp.x) + CUDART_PI_F;
+    float angle = atan2f(disp.y, disp.x);
+    if(angle < 0)
+        angle += 2 * CUDART_PI_F;
+    return angle;
 }
