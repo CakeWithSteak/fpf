737349d070b1e7b11bcc6cc506abe9078cc73839
Violet <cakewithsteak@gmail.com>
2020-04-04 16:02:06 +0200

    Refactor state

--- controls.h
+++ controls.h
@@ -1,30 +1,28 @@
 #pragma once
 #include "utils/Input.h"
 #include "utils/ModeInfo.h"
+#include "utils/State.h"
 
-InputHandler initControls(Window& window,
-                          Viewport& viewport,
-                          const ModeInfo& mode,
-                          int& maxIters, float& metricArg, std::complex<float>& p, bool& colorCutoffEnabled, float& colorCutoff
-                          ) {
+InputHandler initControls(State& s, RuntimeState& rs) {
     constexpr float MOVE_STEP = 0.8f;
     constexpr float ZOOM_STEP = 0.4f;
     constexpr int ITER_STEP = 2;
     constexpr float PARAM_STEP = 0.05f;
     constexpr float COLOR_CUTOFF_STEP = 1.0f;
 
-    InputHandler in(window);
-    in.addViewport(viewport, GLFW_KEY_UP, GLFW_KEY_DOWN, GLFW_KEY_LEFT, GLFW_KEY_RIGHT, GLFW_KEY_KP_ADD, GLFW_KEY_KP_SUBTRACT, GLFW_KEY_HOME, MOVE_STEP, ZOOM_STEP);
+    InputHandler in(rs.window);
+    in.addViewport(s.viewport, GLFW_KEY_UP, GLFW_KEY_DOWN, GLFW_KEY_LEFT, GLFW_KEY_RIGHT, GLFW_KEY_KP_ADD, GLFW_KEY_KP_SUBTRACT, GLFW_KEY_HOME, MOVE_STEP, ZOOM_STEP);
+    auto& mode = s.mode;
 
     if(!mode.disableIterations)
-        in.addScalar(maxIters, GLFW_KEY_KP_MULTIPLY, GLFW_KEY_KP_DIVIDE, ITER_STEP, "Max itertations", 1, 1024);
+        in.addScalar(s.maxIters, GLFW_KEY_KP_MULTIPLY, GLFW_KEY_KP_DIVIDE, ITER_STEP, "Max itertations", 1, 1024);
 
     if(!mode.disableArg)
-        in.addScalar(metricArg, GLFW_KEY_0, GLFW_KEY_9, mode.argStep, mode.argDisplayName, mode.argMin, mode.argMax);
+        in.addScalar(s.metricArg, GLFW_KEY_0, GLFW_KEY_9, mode.argStep, mode.argDisplayName, mode.argMin, mode.argMax);
 
-    in.addScalar(p, GLFW_KEY_D, GLFW_KEY_A, {PARAM_STEP}, "Parameter");
-    in.addScalar(p, GLFW_KEY_W, GLFW_KEY_S, {0, PARAM_STEP}, "Parameter");
-    in.addScalar(colorCutoff, GLFW_KEY_C, GLFW_KEY_X, COLOR_CUTOFF_STEP, "Color cutoff", 0.0f);
-    in.addToggle(colorCutoffEnabled, GLFW_KEY_Z, "Color cutoff");
+    in.addScalar(s.p, GLFW_KEY_D, GLFW_KEY_A, {PARAM_STEP}, "Parameter");
+    in.addScalar(s.p, GLFW_KEY_W, GLFW_KEY_S, {0, PARAM_STEP}, "Parameter");
+    in.addScalar(s.colorCutoff, GLFW_KEY_C, GLFW_KEY_X, COLOR_CUTOFF_STEP, "Color cutoff", 0.0f);
+    in.addToggle(s.colorCutoffEnabled, GLFW_KEY_Z, "Color cutoff");
     return in;
 }
---
+++ utils/State.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <string>
+#include <complex>
+#include "../Computation/kernel_types.h"
+#include "Viewport.h"
+#include "ModeInfo.h"
+#include "../Rendering/Window.h"
+#include "../Rendering/Renderer.h"
+#include "Input.h"
+#include "../cli.h"
+
+//A struct encompassing program state useful for event handlers and serialization
+struct State {
+    std::string expr;
+    int maxIters = 128;
+    float metricArg;
+    std::complex<float> p{0};
+    Viewport viewport;
+    bool colorCutoffEnabled;
+    float colorCutoff;
+    int width, height;
+    ModeInfo mode; //Only the DistanceMetric is serialized
+
+    State(const Options& opt) {
+        expr = opt.expression;
+        mode = opt.mode;
+        width = opt.width;
+        height = opt.height;
+        metricArg = mode.argInitValue;
+        colorCutoffEnabled = (mode.defaultColorCutoff != -1);
+        colorCutoff = colorCutoffEnabled ? mode.defaultColorCutoff : 10.0f;
+    }
+};
+
+struct RuntimeState {
+    Window& window;
+    Renderer& renderer;
+};
