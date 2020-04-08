23c9ec38e5c0c7e09234b74f4ed4f539571cb45d
Vince <cakewithsteak@gmail.com>
2020-04-08 22:24:19 +0200

    Change path coloring

--- Rendering/shaders/overlay_frag.glsl
+++ Rendering/shaders/overlay_frag.glsl
@@ -4,10 +4,18 @@ R"(
 in float index;
 out vec4 fragColor;
 
-const vec4 lineColor = vec4(0.05, 0.2, 0.8, 1);
+const vec4 lineColorStart = vec4(0.05, 0.2, 0.8, 1);
+const vec4 lineColorEnd = vec4(0.25, 1, 0.9, 1);
+const float mixCoeff = 10;
+const float arrowLength = 0.1f; // Adds helpful "arrows" pointing towards the next step of the path.
 
 void main() {
-    fragColor = lineColor + vec4(0, index / 10, 0, 0);
+    int flatIndex = int(index);
+    if(abs(index - flatIndex) < arrowLength) {
+        fragColor = (flatIndex % 2 == 0) ? vec4(1, 0, 0.3, 1) : vec4(0.3, 0, 1, 1);
+    } else {
+        fragColor = mix(lineColorStart, lineColorEnd, (index / mixCoeff));
+    }
 }
 
 )"
