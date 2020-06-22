8568735c31e424a7bf2d1dfcb7218a73f0e2ecd9
vince <cakewithsteak@gmail.com>
2020-06-22 16:59:47 +0200

    Remove binary operations

--- Compilation/expr2cuda/operators.h
+++ Compilation/expr2cuda/operators.h
@@ -8,11 +8,6 @@ const std::map<std::string, OperatorTraits> operatorLookup {
         {"*", {"*", false, 2, "cmul", LEFT_ASSOCIATIVE, 2}},
         {"/", {"/", false, 2, "cdiv", LEFT_ASSOCIATIVE, 2}},
 
-        {"or", {"or", true, 2, "cor"}},
-        {"xor", {"xor", true, 2, "cxor"}},
-        {"and", {"and", true, 2, "cand"}},
-        {"not", {"not", true, 1, "cnot"}},
-
         {"sin", {"sin", true, 1, "csin"}},
         {"cos", {"and", true, 1, "ccos"}},
         {"tan", {"tan", true, 1, "ctan"}},
--- Computation/math_float.cuh
+++ Computation/math_float.cuh
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "kernel_macros.cuh"
-#include "kernel_stdint.cuh"
 #include "kernel_types.cuh"
 
 RUNTIME #include <cuComplex.h>
@@ -39,54 +38,6 @@ __device__ __inline__ complex cneg(complex z) {
     return make_complex(
             -z.x,
             -z.y
-    );
-}
-
-__device__ __inline__ complex cnot(complex z) {
-    uint64_t re = ~*reinterpret_cast<uint32_t*>(&z.x);
-    uint64_t im = ~*reinterpret_cast<uint32_t*>(&z.y);
-    return make_complex(
-            *reinterpret_cast<float*>(&re),
-            *reinterpret_cast<float*>(&im)
-    );
-}
-
-__device__ __inline__ complex cor(complex a, complex b) {
-    uint64_t re1 = ~*reinterpret_cast<uint32_t*>(&a.x);
-    uint64_t im1 = ~*reinterpret_cast<uint32_t*>(&a.y);
-    uint64_t re2 = ~*reinterpret_cast<uint32_t*>(&b.x);
-    uint64_t im2 = ~*reinterpret_cast<uint32_t*>(&b.y);
-    uint64_t re = re1 | re2;
-    uint64_t im = im1 | im2;
-    return make_complex(
-            *reinterpret_cast<float*>(&re),
-            *reinterpret_cast<float*>(&im)
-    );
-}
-
-__device__ __inline__ complex cand(complex a, complex b) {
-    uint64_t re1 = ~*reinterpret_cast<uint32_t*>(&a.x);
-    uint64_t im1 = ~*reinterpret_cast<uint32_t*>(&a.y);
-    uint64_t re2 = ~*reinterpret_cast<uint32_t*>(&b.x);
-    uint64_t im2 = ~*reinterpret_cast<uint32_t*>(&b.y);
-    uint64_t re = re1 & re2;
-    uint64_t im = im1 & im2;
-    return make_complex(
-            *reinterpret_cast<float*>(&re),
-            *reinterpret_cast<float*>(&im)
-    );
-}
-
-__device__ __inline__ complex cxor(complex a, complex b) {
-    uint64_t re1 = ~*reinterpret_cast<uint32_t*>(&a.x);
-    uint64_t im1 = ~*reinterpret_cast<uint32_t*>(&a.y);
-    uint64_t re2 = ~*reinterpret_cast<uint32_t*>(&b.x);
-    uint64_t im2 = ~*reinterpret_cast<uint32_t*>(&b.y);
-    uint64_t re = re1 ^ re2;
-    uint64_t im = im1 ^ im2;
-    return make_complex(
-            *reinterpret_cast<float*>(&re),
-            *reinterpret_cast<float*>(&im)
     );
 }
 
