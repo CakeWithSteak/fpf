d176c5547ebc8cb3042e818d6e6c1ccacf3fb364
Violet <cakewithsteak@gmail.com>
2020-07-03 15:21:50 +0200

    Fix bug where orbit tracking would stop prematurely in float mode

--- Rendering/Renderer.h
+++ Rendering/Renderer.h
@@ -65,7 +65,7 @@ class Renderer {
     static constexpr size_t PERF_LINE_TRANS_GEN = 4;
     static constexpr size_t PERF_ATTRACTOR = 5;
 
-    static constexpr int MAX_PATH_STEPS = 256;
+    static constexpr int MAX_PATH_STEPS = 512;
     static constexpr double PATH_PARAM_UPDATE_THRESHOLD = 0.01f;
     static constexpr double PATH_TOL_UPDATE_THRESHOLD = 0.001f;
     static constexpr double DEFAULT_PATH_TOLERANCE = 0.001f;
